5d7afe8370a6e5df6821b4abdb3131057917770b
Sapan <sapantanted99@gmail.com>
2019-09-21 12:06:50 +0530

    changed queries to plot, changed cachesystem.json, added week granularity, changed submit script ,paper submitted in CoDS COMAD 2020

--- build_and_submit.sh
+++ build_and_submit.sh
@@ -1,3 +1,3 @@
 mvn clean package
-spark-submit --driver-memory 3g --driver-java-options "-ea" --class main.Main --master spark://10.129.149.40:7077  --conf spark.driver.host=10.129.149.32 --conf spark.executor.cores=2 --conf spark.cores.max=12 --conf spark.executor.memory=4g --jars target/CachingSystem-1.0-SNAPSHOT.jar,target/dependency/* --deploy-mode client target/CachingSystem-1.0-SNAPSHOT-jar-with-dependencies.jar ./cachesystem.json
+spark-submit --driver-memory 4g --driver-java-options "-ea" --class main.Main --master spark://10.129.6.60:7077  --conf spark.driver.host=10.129.149.32 --conf spark.executor.cores=2 --conf spark.cores.max=6 --conf spark.executor.memory=4g --jars target/CachingSystem-1.0-SNAPSHOT.jar,target/dependency/* --deploy-mode client target/CachingSystem-1.0-SNAPSHOT-jar-with-dependencies.jar ./cachesystem.json
 
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,13 +15,13 @@ import java.util.List;
 import static managers.Utils.sparkLogsOff;
 
 public class Main {
-    public static boolean runPredefinedQueries=false;
-    public static boolean serialQueryExecution = false;
-    public static boolean singleTimeExecution = false;
-    public static boolean clearCacheSystemInitially = false;
+    public static boolean runPredefinedQueries=true;
+    public static boolean serialQueryExecution = true;
+    public static boolean singleTimeExecution = true;
+    public static boolean clearCacheSystemInitially = true;
     public static boolean clearCacheSystemAfterEveryQuery=false;
-    public static boolean logExecutionStatistics=false;
-    private static String queryType = "zoomOutQueries";//zoomInQueries,zoomOutQueries,slideQueries
+    public static boolean logExecutionStatistics=true;
+    private static String queryType = "zoomOutQueries";//zoomOutQueries,zoomInQueries,slideQueries
 
     public static void main(String[] args) {
         if (args.length <= 0) {
@@ -45,45 +45,51 @@ public class Main {
 //    SELECT FLOOR(TS) as time_sec, power as value,'power_sensor' as metric, 'from_cache' as meta_data FROM power WHERE   TS >= 1514745000 AND TS <= 1514831400 and sensor_id ='power_k_seil_p' and granularity like '1 hour' order by time_sec
 
     public static List<QueryBean> getNewQueries() {
-        ArrayList<String> zoomInQueries = new ArrayList<>();
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1451586600 AND TS <= 1540060200 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1527445800 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1505327400 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1494268200 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1488738600 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1485973800 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1484591400 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1483900200 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1483554600 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1483381800 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1483295400 and sensor_id ='power_k_seil_p' order by time_sec");
-
         ArrayList<String> zoomOutQueries = new ArrayList<>();
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1483295400 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1483381800 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1483554600 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1483900200 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1484591400 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1485973800 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1488738600 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1494268200 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1505327400 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1527445800 and sensor_id ='power_k_seil_p' order by time_sec");
-        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1451586600 AND TS <= 1540060200 and sensor_id ='power_k_seil_p' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1325356200 AND TS <= 1325442600 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 946751400 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 946837800 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 947010600 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 947356200 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 948047400 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 949429800 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 952194600 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 957724200 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 968783400 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 990901800 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 1035138600 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 1123612200 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomOutQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 1300559400 and sensor_id ='power_k_sr_a' order by time_sec");
+
+        ArrayList<String> zoomInQueries = new ArrayList<>();
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1325356200 AND TS <= 1325442600 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 1300559400 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 1123612200 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 1035138600 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 990901800 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 968783400 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 957724200 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 952194600 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 949429800 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 948047400 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 947356200 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 947010600 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 946837800 and sensor_id ='power_k_sr_a' order by time_sec");
+        zoomInQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_sr_a' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 946665000 AND TS <= 946751400 and sensor_id ='power_k_sr_a' order by time_sec");
 
         ArrayList<String> slideQueries = new ArrayList<>();//correct queries
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1514745000 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1498933800 AND TS <= 1530469800 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1467397800 AND TS <= 1498933800 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1519842600 AND TS <= 1522434600 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1521138600 AND TS <= 1523730600 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1518546600 AND TS <= 1521138600 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1527877800 AND TS <= 1527964200 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1527921000 AND TS <= 1528007400 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1527834600 AND TS <= 1527921000 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1533079800 AND TS <= 1533097800 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1533088800 AND TS <= 1533106800 and sensor_id ='power_k_seil_p' order by time_sec");
-        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1533070800 AND TS <= 1533088800 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1483209000 AND TS <= 1514745000 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1498933800 AND TS <= 1530469800 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1467397800 AND TS <= 1498933800 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1519842600 AND TS <= 1522434600 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1521138600 AND TS <= 1523730600 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1518546600 AND TS <= 1521138600 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1527877800 AND TS <= 1527964200 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1527921000 AND TS <= 1528007400 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1527834600 AND TS <= 1527921000 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1533079800 AND TS <= 1533097800 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1533088800 AND TS <= 1533106800 and sensor_id ='power_k_seil_p' order by time_sec");
+//        slideQueries.add("SELECT FLOOR(TS) as time_sec, power as value,'power_k_seil_p' as metric, 'from_cache' as meta_data FROM power WHERE TS >= 1533070800 AND TS <= 1533088800 and sensor_id ='power_k_seil_p' order by time_sec");
 
         ArrayList<String> adhocQueries = new ArrayList<>();
         adhocQueries.add("SELECT FLOOR(TS) as time_sec, power as value, 'power_k_a' as metric, 'from_cache' as ignore_cache FROM power WHERE TS >= 1546281000 AND TS <= 1546453800 and sensor_id ='power_k_a' and granularity like '%' order by time_sec");
